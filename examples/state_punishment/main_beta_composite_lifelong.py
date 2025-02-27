b30c7ad699e329fa382e5401c987b2bcd0bf70b2
Yikai <93346850+Yikai369@users.noreply.github.com>
2025-02-27 16:24:08 -0500

    update, lifetime training

--- examples/state_punishment/main_beta_composite_lifelong.py
+++ examples/state_punishment/main_beta_composite_lifelong.py
@@ -65,9 +65,8 @@ def run(cfg, **kwargs):
     transgression_punishment_record = pd.DataFrame(columns=['agent', 'transgression', 'punished', 'time'])
 
     # load weights
-    for count, agent in enumerate(agents):
-        agent.model.load(f'{root}/examples/state_punishment/models/checkpoints/test_voting_stacked_views_composite_actions_3agents_v0_agent{agent.ixs}_iRainbowModel.pkl')
-    
+    # for count, agent in enumerate(agents):
+    #     agent.model.load(f'{root}/examples/state_punishment/models/checkpoints/fixed_punishment_rate_0.0_0.0_1.0_3As_size15_3Resources_ambiguity_v2_init0.2_agent{count}_iRainbowModel.pkl')
     # If a path to a model is specified in the run, load those weights
     if "load_weights" in kwargs:
         for agent in agents:
@@ -84,24 +83,27 @@ def run(cfg, **kwargs):
     elif cfg.action_mode == 'simple':
         assert cfg.model.iqn.parameters.action_size == 6, ValueError('Number of actions should be 6 when the action mode is simple')
     
-
-    for epoch in range(cfg.experiment.epochs):
-
-        # initialize state system
-        
-        state_entity = state_sys(
+    # initialize state system
+    state_entity = state_sys(
             cfg.state_sys.init_prob, 
             fixed_prob_dict,
             cfg.state_sys.magnitude, 
             cfg.state_sys.taboo,
             cfg.state_sys.change_per_vote
             )
+    
+    state_mode = cfg.state_mode
+    for env in envs:
+        env.reset(state_mode=state_mode)
+        env.cache['harm'] = [0 for _ in range(len(agents))]
 
-        state_mode = cfg.state_mode
+    for epoch in range(cfg.experiment.epochs):
+
+        
         # Reset the environment at the start of each epoch
-        for env in envs:
-            env.reset(state_mode=state_mode)
-            env.cache['harm'] = [0 for _ in range(len(agents))]
+        # for env in envs:
+        #     env.reset(state_mode=state_mode)
+        #     env.cache['harm'] = [0 for _ in range(len(agents))]
         # for agent in env.agents:
         #     agent.reset()
         random.shuffle(agents)
@@ -114,7 +116,7 @@ def run(cfg, **kwargs):
         # Container for data within epoch
         punishment_increase_record = [0 for _ in range(len(agents))]
         punishment_decrease_record = [0 for _ in range(len(agents))]
-        action_record = [0 for _ in range(cfg.model.iqn.parameters.action_size)]
+        action_record = [[0 for _ in range(cfg.model.iqn.parameters.action_size)] for _ in range(len(agents))]
 
         while not done:
 
@@ -148,7 +150,7 @@ def run(cfg, **kwargs):
                 
                 # record actions
                 action_record[agent.ixs][action] += 1
-
+                
                 # composite_state = agent.generate_composite_state(envs)
                 # record voting behaviors
                 if action == 4:
@@ -157,16 +159,17 @@ def run(cfg, **kwargs):
                     punishment_decrease_record[agent.ixs] += 1 
 
                 # agent.add_memory(state, action, reward, done)
-
-                if turn >= cfg.experiment.max_turns or done_:
-                    done = 1
-                    # agent.add_final_memory(next_state)
-
                 agent.add_memory(state, action, reward, done)
+
+                agent.model.end_epoch_action(**locals())
+                
+                if turn >= cfg.experiment.max_turns or done_:
+                    done = 1 # lifelong
+                    # agent.add_final_memory(next_state)                
 
                 game_points[agent.ixs] += float(reward)
 
-                agent.model.end_epoch_action(**locals())
+                
 
         # At the end of each epoch, train as long as the batch size is large enough.
         for agent in agents:
@@ -207,6 +210,7 @@ def run(cfg, **kwargs):
                     {f'action_{k}': action_record[agent.ixs][k] for k in range(cfg.model.iqn.parameters.action_size)},
                     epoch
                 )
+              
             writer.add_scalar(f'state_punishment_level_avg', np.mean(state_entity.prob_record), epoch)
             writer.add_scalar(f'state_punishment_level_end', state_entity.prob, epoch)
             writer.add_scalar(f'state_punishment_level_init', state_entity.init_prob, epoch)
@@ -215,6 +219,15 @@ def run(cfg, **kwargs):
         for agent in agents:
             new_epsilon = agent.model.epsilon - cfg.experiment.epsilon_decay
             agent.model.epsilon = max(new_epsilon, 0.01)
+
+            # clear encounter record
+            agent.encounters = {
+                'Gem': 0,
+                'Coin': 0,
+                'Food': 0,
+                'Bone': 0,
+                'Wall': 0
+            }
 
 
         if (epoch % 1000 == 0) or (epoch == cfg.experiment.epochs - 1):
@@ -229,8 +242,8 @@ def run(cfg, **kwargs):
                                     f'{cfg.root}/examples/state_punishment/models/checkpoints/{cfg.exp_name}_agent{a_ixs}_{cfg.model.iqn.type}.pkl'
                                     )
                     
-        # epoch_transgression_df = build_transgression_and_punishment_record(agents)
-        # transgression_punishment_record = pd.concat([transgression_punishment_record, epoch_transgression_df], ignore_index=True)
+        epoch_transgression_df = build_transgression_and_punishment_record(agents)
+        transgression_punishment_record = pd.concat([transgression_punishment_record, epoch_transgression_df], ignore_index=True)
         for agent in agents:
             agent.reset_record()
 
@@ -240,7 +253,7 @@ def run(cfg, **kwargs):
         writer.close()
 
     # save punishment record
-    # transgression_punishment_record.to_csv(f'data/{cfg.exp_name}_transgression_record.csv')
+    transgression_punishment_record.to_csv(f'data/{cfg.exp_name}_transgression_record.csv')
 
 def main():
     import argparse
